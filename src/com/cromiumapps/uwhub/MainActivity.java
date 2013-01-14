0fb8ff0b480e382c1ae932bbccf2e35f92c6ffd7
Adrian Louie Gaw <gawadrian@gmail.com>
2013-01-14 00:43:29 -0500

    Basic UWAPIWrapper Implementation. No JSON parser yet.

--- src/com/cromiumapps/uwhub/MainActivity.java
+++ src/com/cromiumapps/uwhub/MainActivity.java
@@ -2,6 +2,7 @@ package com.cromiumapps.uwhub;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_main);
+	
+	Intent iMainMenu = new Intent("com.cromiumapps.uwhub.MAINMENU");
+	startActivity(iMainMenu);
+	finish();
     }
 
     @Override
---
+++ src/com/cromiumapps/uwhub/MainMenu.java
@@ -0,0 +1,38 @@
+package com.cromiumapps.uwhub;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainMenu extends ListActivity {
+    
+    	//What to show
+	String pages[] = {"API Test Caller", "Example 2"};
+	//Copy name in manifest. Case sensitive.
+	String values[] = {"APITestCaller", "Example2"};
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(MainMenu.this, android.R.layout.simple_list_item_1, pages));
+		
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		String choice = values[position];
+		super.onListItemClick(l, v, position, id);
+		
+		try{
+		Class linkClass = Class.forName("com.cromiumapps.uwhub." + choice);
+		Intent ourIntent = new Intent(MainMenu.this, linkClass);
+		startActivity(ourIntent);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}

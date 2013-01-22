667a4b1afad52cac229ba9946ca858d878267f2e
Adrian Louie Gaw <gawadrian@gmail.com>
2013-01-21 23:00:35 -0500

    Huge Update Part 2. Fixed API_KEY bug and Main Menu.

--- src/com/cromiumapps/uwhub/OMGUW.java
+++ src/com/cromiumapps/uwhub/OMGUW.java
@@ -37,47 +37,45 @@ public class OMGUW extends Activity implements UWAPIWrapperListener {
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_omguw);
-	
+
 	lvOMGUWContent = (ListView) findViewById(R.id.lvOMGUWContent);
-	
+
 	OMGUWListadapter = new OMGUWListAdapter(this, omguws);
 	lvOMGUWContent.setAdapter(OMGUWListadapter);
-	
+
 	// Initialize an API wrapper object using the API key and this activity
 	// as the listener.
 	API_KEY = ((UWHUB) this.getApplication()).getAPI_KEY();
 	apiWrapper = new UWAPIWrapper(API_KEY, this);
-	
-	//Call Service
+
+	// Call Service
 	apiWrapper.callService("OMGUW");
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(R.menu.activity_omguw, menu);
-	return true;
+		public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_omguw, menu);
+		return true;
     }
 
     @Override
     public void onUWAPIRequestComplete(JSONObject jsonObject, boolean success) {
-	try {
-		JSONObject jsonResponse = jsonObject.getJSONObject("response");
-		JSONObject jsonData = jsonResponse.getJSONObject("data");
-		JSONObject jsonOMG = jsonData.getJSONObject("OMG");
-		JSONArray jsonResult = jsonOMG.getJSONArray("result");
-		
-		for (int i = 0; i < jsonResult.length(); i++) {
-			JSONObject omguwObject = jsonResult.getJSONObject(i);
-			omguws.add(new OMGUWData(omguwObject.getString("ID"), omguwObject.getString("Date"), omguwObject.getString("Content"), omguwObject.getString("Link"), omguwObject.getString("Type")));
+		try {
+			JSONObject jsonResponse = jsonObject.getJSONObject("response");
+			JSONObject jsonData = jsonResponse.getJSONObject("data");
+			JSONObject jsonOMG = jsonData.getJSONObject("OMG");
+			JSONArray jsonResult = jsonOMG.getJSONArray("result");
+			
+			for (int i = 0; i < jsonResult.length(); i++) {
+				JSONObject omguwObject = jsonResult.getJSONObject(i);
+				omguws.add(new OMGUWData(omguwObject.getString("ID"), omguwObject.getString("Date"), omguwObject.getString("Content"), omguwObject.getString("Link"), omguwObject.getString("Type")));
+			}
+			
+			OMGUWListadapter.setData(omguws);
+			OMGUWListadapter.notifyDataSetChanged();
+		} catch (JSONException e) {
+			Log.v(LOG_TAG, e.getMessage());
 		}
-		
-		OMGUWListadapter.setData(omguws);
-		OMGUWListadapter.notifyDataSetChanged();
-	} catch (JSONException e) {
-		Log.v(LOG_TAG, e.getMessage());
-	}
-	
     }
-
 }
